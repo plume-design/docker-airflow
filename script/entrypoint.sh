f54df0c45e55cb6d05ca3b4fb9a8c7acf91e5194
Teng Peng <josephtengpeng@gmail.com>
2018-10-28 15:18:04 -0700

    Update entrypoint.sh

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -2,11 +2,11 @@
 
 TRY_LOOP="20"
 
-: "${REDIS_HOST:="redis"}"
+: "${REDIS_HOST:="10.83.32.131"}"
 : "${REDIS_PORT:="6379"}"
 : "${REDIS_PASSWORD:=""}"
 
-: "${POSTGRES_HOST:="postgres"}"
+: "${POSTGRES_HOST:="10.83.32.131"}"
 : "${POSTGRES_PORT:="5432"}"
 : "${POSTGRES_USER:="airflow"}"
 : "${POSTGRES_PASSWORD:="airflow"}"
